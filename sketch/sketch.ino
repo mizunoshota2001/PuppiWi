20dba2668e652b0f5801668c9fd987ecfc57d117
mizunoshota2001 <95399151+mizunoshota2001@users.noreply.github.com>
2024-09-02 19:38:46 +0900

    2024年9月2日

---
+++ sketch/sketch.ino
@@ -0,0 +1,15 @@
+#include <Servo.h>
+
+Servo myServo;  // Servoオブジェクトを作成
+
+void setup() {
+  myServo.attach(9);  // サーボモーターの信号線をデジタルピン9に接続
+}
+
+void loop() {
+  myServo.write(0);    // サーボを0度に設定
+  delay(2500);         // 1秒待つ
+
+  myServo.write(90);   // サーボを90度に設定
+  delay(2500);         // 1秒待つ
+}
