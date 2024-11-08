867c5a0b82a45cbed83ce2581bb513f90c37fb40
mizunoshota2001 <95399151+mizunoshota2001@users.noreply.github.com>
2024-11-08 15:52:06 +0900

    Raspberry Pi setup script: add service configuration and enable startup service

--- RaspberryPi/assets/build.sh
+++ RaspberryPi/assets/build.sh
@@ -2,9 +2,16 @@
 
 sudo apt install -y git python3-pip python3-venv
 mkdir app
+
 cd /home/puppiwi/app
 python3 -m venv .venv
 .venv/bin/python -m pip install flask rpi.gpio
+
+sudo touch /etc/systemd/system/puppiwi.service
+sudo cp -a  assets/puppiwi.service /etc/systemd/system/puppiwi.service
+sudo systemctl daemon-reload
+sudo systemctl enable startup.service
+
 cd /home/puppiwi
 git clone https://github.com/mizunoshota2001/PuppiWi.git tmp
 cp -a tmp/RaspberryPi/* app
