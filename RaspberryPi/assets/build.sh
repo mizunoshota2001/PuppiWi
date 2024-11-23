ac6476193784bc48fb862609f7eb01b545446e61
mizunoshota2001 <95399151+mizunoshota2001@users.noreply.github.com>
2024-11-23 20:50:08 +0900

    README.mdとbuild.shを更新し、スクリプトのURLをpi5ブランチに変更しました。また、build.sh内のgit cloneコマンドをpi5ブランチを指定するように修正しました。

--- RaspberryPi/assets/build.sh
+++ RaspberryPi/assets/build.sh
@@ -8,12 +8,12 @@ python3 -m venv .venv
 .venv/bin/python -m pip install flask rpi-lgpio
 
 sudo touch /etc/systemd/system/puppiwi.service
-sudo cp -a  assets/puppiwi.service /etc/systemd/system/puppiwi.service
+sudo cp -a assets/puppiwi.service /etc/systemd/system/puppiwi.service
 sudo systemctl daemon-reload
 sudo systemctl unmask puppiwi.service
 sudo systemctl enable puppiwi.service
 
 cd /home/puppiwi
-git clone https://github.com/mizunoshota2001/PuppiWi.git tmp
+git clone https://github.com/mizunoshota2001/PuppiWi.git -b pi5 tmp
 cp -a tmp/RaspberryPi/* app
 rm -rf tmp
