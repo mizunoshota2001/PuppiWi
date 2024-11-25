f42118cffb8813c237659220aa0db0345a8829cc
mizunoshota2001 <95399151+mizunoshota2001@users.noreply.github.com>
2024-11-25 18:24:26 +0900

    virtualCamera: update modprobe command to include max_buffers and change card label

--- RaspberryPi/assets/virtualCamera.md
+++ RaspberryPi/assets/virtualCamera.md
@@ -3,5 +3,5 @@ v4l2-ctl --list-formats-ext
 
 v4l2-ctl -v width=176,height=144,pixelformat=MJPG
 sudo modprobe v4l2loopback -r
-sudo modprobe v4l2loopback video_nr=2 card_label=VirtualCam exclusive_caps=1
+sudo modprobe v4l2loopback video_nr=2 card_label=VirtualCam
 ffmpeg -f v4l2 -framerate 5 -i /dev/video0 -f v4l2 -r 5 /dev/video2
--- RaspberryPi/assets/virtualCamera.sh
+++ RaspberryPi/assets/virtualCamera.sh
@@ -2,5 +2,5 @@
 
 v4l2-ctl -v width=176,height=144,pixelformat=MJPG
 sudo modprobe v4l2loopback -r
-sudo modprobe v4l2loopback video_nr=2 card_label=VirtualCamera
-ffmpeg -f v4l2 -i /dev/video0 -f v4l2 -r 5 /dev/video2
+sudo modprobe v4l2loopback video_nr=2 card_label=VirtualCam exclusive_caps=1 max_buffers=2
+ffmpeg -f v4l2 -i /dev/video0 -vf format=yuv420p -f v4l2 -r 5 /dev/video2
